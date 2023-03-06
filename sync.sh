4f22c6c2bf72ca37baf9c73394dc2390ce6cf2af
tas509 <126862771+tas509@users.noreply.github.com>
2023-03-06 16:07:07 +0000

    delete env

--- sync.sh
+++ sync.sh
@@ -1,1 +1,1 @@
-rsync -avuzr  /Users/tomsmith/Desktop/sql/www tomsmith@opal11.opalstack.com:/home/tomsmith/apps/newflask;echo http://flask.everythingability.opalstacked.com/
+rsync -avuzr  /Users/tomsmith/Desktop/sql/www tomsmith@opal11.opalstack.com:/home/tomsmith/apps/flask;echo http://flask.everythingability.opalstacked.com/
